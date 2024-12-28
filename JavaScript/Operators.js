5f39ee2b7eacecaafdebaf087f07fbbd37407201
krishnakumar <krishnakuma.s1973@gmail.com>
2024-12-28 11:27:37 +0530

    second commit

--- JavaScript/Operators.js
+++ JavaScript/Operators.js
@@ -21,3 +21,14 @@ console.log(x != y);
 console.log(x > 5 && y > 5);
 console.log(x > 5 || y > 5);
 
+//Bitwise Operators
+console.log(x & y);
+console.log(x | y);
+console.log(x ^ y);
+console.log(~x);
+console.log(x << y);
+console.log(x >> y);
+
+//Ternary Operators
+let z = x > y ? x : y;
+console.log(z);
